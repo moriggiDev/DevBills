8eaee387ecbdbec9b0564e0bff53423847e9f6ae
moriggiDev <badigital42@gmail.com>
2026-02-04 07:28:36 -0300

    primeiro commit com server rodando

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import Fastify from "fastify";
+import type { FastifyInstance } from "fastify";
+import routes from "./routes/index.js";
+
+const app: FastifyInstance = Fastify({
+  logger: true,
+});
+
+app.register(routes);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import app from "./app.js";
+
+const PORT = 3001;
+
+const startServer = async () => {
+  try {
+    await app
+      .listen({ port: PORT })
+      .then(() => console.log(`Servidor rodando na porta ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+startServer();
